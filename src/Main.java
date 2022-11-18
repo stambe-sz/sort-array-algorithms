4f776bb0dc56506e270499599a95075580a69314
Stanimir Stoychev <52385541+stambe-sz@users.noreply.github.com>
2022-11-18 02:04:27 +0200

    .

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {54, 30, 15, 5, 10, 25, 60};
+        mergeSort(arr, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    public  static void mergeSort(int[] arr,int arrLength){
+
+    }
+    }
+}
