d551234607d7f5ffeee45649a27c5767d7fc6f37
Stanimir Stoychev <52385541+stambe-sz@users.noreply.github.com>
2022-11-18 02:24:49 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,30 @@ public class Main {
         int[] arr = {54, 30, 15, 5, 10, 25, 60};
         mergeSort(arr, arr.length);
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 
-    public  static void mergeSort(int[] arr,int arrLength){
+    public static void mergeSort(int[] arr, int arrLength) {
 
-    }
+        if (arrLength < 2){
+            return;
+        }
+        int middle = arrLength / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[arrLength - middle];
+
+        int k = 0;
+        for (int i = 0; i < arrLength; i++) {
+            if (i < middle){
+                leftArray[i] = arr[i];
+            }else {
+                rightArray[k] = arr[i];
+                k++;
+            }
+        }
+        mergeSort(leftArray,middle);
+        mergeSort(rightArray,arrLength - middle);
     }
 }
+
