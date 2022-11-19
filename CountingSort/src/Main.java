eb38cc3c64d2408082a63966eb7bc47174d6fcc4
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2022-11-19 16:07:45 +0200

    .

---
+++ CountingSort/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {54, 30, 15, 5, 10, 25, 60};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
