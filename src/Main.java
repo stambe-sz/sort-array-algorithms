7b76319b6eb31b578b5f1a9126fc28c96a0609de
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2022-11-19 08:37:06 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+//merge sort
 public class Main {
     public static void main(String[] args) {
         int[] arr = {54, 30, 15, 5, 10, 25, 60};
---
+++ src/QuickSort.java
@@ -0,0 +1,6 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {54, 30, 15, 5, 10, 25, 60};
+        
+    }
+}
