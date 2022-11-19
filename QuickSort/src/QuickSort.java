1099b232e9eadfd0a8edd8b15100493256075d2d
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2022-11-19 15:41:52 +0200

    .

--- QuickSort/src/QuickSort.java
+++ QuickSort/src/QuickSort.java
@@ -10,7 +10,7 @@ public class QuickSort {
         }
     }
 
-    private static void quickSort(int[] array, int low, int high) {
+    private static void quickSort(int array[], int low, int high) {
         if (low < high) {
             int pivotElement = partition(array, low, high);
             quickSort(array, low, pivotElement - 1);
@@ -18,7 +18,7 @@ public class QuickSort {
         }
     }
 
-    private static int partition(int[] array, int low, int high) {
+    private static int partition(int array[], int low, int high) {
         int pivot = array[high];
 
         int i = low - 1;
@@ -30,6 +30,9 @@ public class QuickSort {
                 array[j] = temp;
             }
         }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
 
         return i + 1;
     }
