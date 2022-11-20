db4c5a9ef44b3d3a974849c8937e0c3709510621
Stanimir Stoychev <52385541+stambe-sz@users.noreply.github.com>
2022-11-20 16:38:42 +0200

    .

---
+++ BubbleSort/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {54, 30, 15, 5, 10, 25, 60};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
